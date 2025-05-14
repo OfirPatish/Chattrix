bce3014ebda654e7ccbb7118a79062c23203dfe0
Ofir Patish <ofirpatishop@gmail.com>
2025-05-14 12:54:00 +0300

    Fixed location of folder

--- backend/src/server.js
+++ backend/src/server.js
@@ -49,9 +49,9 @@ app.use("/api/auth", apiLimiter, authRoute);
 app.use("/api/messages", apiLimiter, messageRoute);
 
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname, "../../frontend/dist")));
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname, "../../frontend", "dist", "index.html"));
+    res.sendFile(path.resolve(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
